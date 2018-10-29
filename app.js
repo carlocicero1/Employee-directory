364a73769d530b7dea00c0761f17ee407da98b45
Carlo Cicero <carlocicero1@gmail.com>
2018-10-29 16:51:23 -0400

    - Initial commit of working app

---
+++ app.js
@@ -0,0 +1,83 @@
+$('.modal-container').hide();
+const listOfEmployees = [];
+
+function appendEmployee (employee) {
+	$('#gallery').append(employee)
+}
+
+// Dynamically creates each generated employee along with information for profile
+function createEmployees () {
+	$.ajax({
+		url: 'https://randomuser.me/api/?results=12',
+		dataType: 'json',
+		success: function (data) {
+			
+			for(let i = 0; i < data.results.length; i++) {
+				const { email, name, location, picture } = data.results[i]
+				listOfEmployees.push(data.results[i]);
+				const newPerson = `
+				<div class="card" data-person="${i}">
+					<div class="card-img-container">
+					<img class="card-img" src="${picture.large}" alt="profile picture">
+						</div>
+						<div class="card-info-container">
+								<h3 id="name" class="card-name cap">${name.first} ${name.last}</h3>
+								<p class="card-text">${email}</p>
+								<p class="card-text cap">${location.city}, ${location.state}</p>
+						</div>
+				</div>`
+
+				appendEmployee(newPerson)
+			}
+			
+			events();
+		},
+		error: function (err) {
+			alert(err, err.statusText);
+		}
+	})
+}
+
+	// React to click event on selected profile and run modal function
+function events () {
+	$('.card').on('click', (e) => {
+		const $target = $(e.currentTarget)
+		const index = $target.data('person')
+		const person = listOfEmployees[index];
+		loadModal(person);
+	
+	})
+
+}
+
+	// Generate info for modal based on selected person object
+function loadModal (person) {
+	
+		$('.modal-container').show();
+		const email = person.email
+			const firstName = person.name.first
+			const lastName = person.name.last
+			const title = person.name.title
+			const phoneNum = person.phone
+			const city = person.location.city
+			const street = person.location.street
+			const state = person.location.state
+			const postcode = person.location.postcode
+			const dateOfBirth = person.dob.date
+			const picture = person.picture.large
+	
+			$('.modal-img').attr('src', picture)
+			$('.email').text(email)
+			$('.modal-name').text(`${title} ${firstName} ${lastName}`);
+			$('.phone').text(phoneNum)
+			$('.city').text(city)
+			$('.address').text(`${street}, ${city}, ${state} ${postcode}`);
+			$('.birthday').text(`Birthday: ${dateOfBirth}`)
+
+}
+
+$('.modal-close-btn').on('click', () => {
+	$('.modal-container').hide();
+})
+
+createEmployees();
